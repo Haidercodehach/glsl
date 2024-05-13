ed95636fef136988fe41db27b351d1c802fe9ec1
Haider Aftab <121552824+Haidercodehach@users.noreply.github.com>
2024-05-13 21:49:36 +0500

    set

--- blahg-main/src/config.js
+++ blahg-main/src/config.js
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "Cassidy's blog template";
-export const SITE_DESCRIPTION = "A blog template for you!";
+export const SITE_TITLE = "GLSL";
+export const SITE_DESCRIPTION = "Help you to learn Shaders";
